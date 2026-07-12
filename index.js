ac21fe49ccba9dc75f59166c12421618d8710587
crloukas98 <crloukas@yahoo.gr>
2026-07-12 09:32:51 +0000

    Final Version All Tests Passed

--- index.js
+++ index.js
@@ -23,7 +23,16 @@ app.get("/", function (req, res) {
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
+app.get("/api", function (req, res) {
 
+  const date = new Date();
+
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
+
+});
 app.get("/api/:date?", function (req, res) {
 
   let date;
